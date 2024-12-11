e5e233223ef0e4cb226e9da7421d2febe7f2d25c
sankalp kumar <65161219+sankalp475@users.noreply.github.com>
2024-12-11 09:11:20 +0000

    new changes

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // const .
     menuBtn.addEventListener('click', function () {
 
-        {/*<ion-icon name=""></ion-icon>*/ }
         if (header.dataset.mobileNavOpen === 'false') {
             header.dataset.mobileNavOpen = true;
             menuBtn.children[0].setAttribute(
@@ -24,17 +23,42 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    document.addEventListener('scroll', function () {
+        if (window.scrollY >= 96) {
+            header.dataset.onScroll = true;
+        } else {
+            header.dataset.onScroll = false;
+        }
+    });
 
 });
-var splide = new Splide('.splide', {
+
+var splide1 = new Splide('.splide1', {
     type: 'loop',
-    autoplay: 'pause',
     perPage: (winWidth < 900 && winWidth > 600) ? 2 : (winWidth < 600) ? 1 : 3,
+    autoplay: true,
+    interval: 9000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+});
+
+splide1.on('autoplay:playing', function () {
+    //   console.log('playing');
+});
+
+splide1.on('autoplay:paused', function () {
+    //   console.log('paused');
 });
 
 
-splide.on('autoplay:playing', function (rate) {
-    console.log(rate); // 0-1
-    
+var splide2 = new Splide('.splide2', {
+    type: 'loop',
+    perPage: 1,
+    autoplay: true,
+    interval: 9000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
 });
-splide.mount();
+
+splide1.mount();
+splide2.mount();
