a915c3b67869a9017be92f3f0cb604e351dbb890
sankalp <sankalp05oct@gmail.com>
2024-12-11 23:21:34 +0530

    new change in js

--- script.js
+++ script.js
@@ -3,22 +3,16 @@ const theme = {
     light: "light-theme",
 }
 
-// document.documentElement.classList.add(JSON.parse( localStorage.getItem("theme")));
-// localStorage.setItem("theme", JSON.stringify(theme));
-// document.documentElement.classList.add(JSON.parse(localStorage.getItem("theme")).light);
-
 let winWidth = window.innerWidth;
 document.addEventListener('DOMContentLoaded', function () {
     const menuBtn = document.querySelector('.mobile-menu-btn');
     const mainNav = document.querySelector('.main-nav');
     const header = document.querySelector('header.header');
 
-    // Apply the saved theme on page load
     const savedTheme = JSON.parse(localStorage.getItem("theme"));
     if (savedTheme) {
         document.documentElement.classList.add(savedTheme);
     } else {
-        // Default to light theme if no theme is saved
         localStorage.setItem("theme", JSON.stringify(theme.light));
         document.documentElement.classList.add(theme.light);
     }
@@ -55,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-var splide1 = new Splide('.splide1', {
+let splide1 = new Splide('.splide1', {
     type: 'loop',
     perPage: (winWidth < 900 && winWidth > 600) ? 2 : (winWidth < 600) ? 1 : 3,
     autoplay: true,
@@ -64,16 +58,7 @@ var splide1 = new Splide('.splide1', {
     pauseOnFocus: true,
 });
 
-splide1.on('autoplay:playing', function () {
-    //   console.log('playing');
-});
-
-splide1.on('autoplay:paused', function () {
-    //   console.log('paused');
-});
-
-
-var splide2 = new Splide('.splide2', {
+let splide2 = new Splide('.splide2', {
     type: 'loop',
     perPage: 1,
     autoplay: true,
